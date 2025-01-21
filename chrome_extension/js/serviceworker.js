d073cf41bc6645ced6e6de39826b586bf248fd75
gilsheps <gil.sheps@gmail.com>
2025-01-21 23:23:19 +0200

    daily backup dev

---
+++ chrome_extension/js/serviceworker.js
@@ -0,0 +1,12 @@
+// window.addEventListener("load", async (_) => {
+//     try {
+//       Preferences.saveSelectedHost({})
+//       console.log('background load')
+//     } catch {}
+//   });
+
+// chrome.runtime.onStartup.addListener(() => {
+//   Performance.saveSelectedHost({});
+//   var host = Performance.getSelectedHost();
+//   console.log("hosttttt", host);
+// });
